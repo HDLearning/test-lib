d89b1486fd2fd229c2d858385124ea7bf15cd443
Hamza <mlearning51@gmail.com>
2025-03-04 16:08:53 +0500

    Update pandas_check.py

--- models/pandas_check.py
+++ models/pandas_check.py
@@ -2,9 +2,8 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-try:
-    import pandas as pd
-    df = pd.DataFrame({"Test": [1, 2, 3]})
-    _logger.info("Pandas is installed and working: %s", df)
-except ImportError:
-    _logger.error("Pandas is NOT installed!")
+
+import pandas as pd
+df = pd.DataFrame({"Test": [1, 2, 3]})
+_logger.info("Pandas is installed and working: %s", df)
+
